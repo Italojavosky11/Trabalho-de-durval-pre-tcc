129203ad5dec52066691fa07cc0ceb167774d52f
Italojavosky11 <silva.camilo@escolar.ifrn.edu.br>
2026-02-06 17:40:23 -0300

    14
    só falta  música adaptativa

--- pretcc/Assets/scripts/Injecaodecura.cs
+++ pretcc/Assets/scripts/Injecaodecura.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class Injecaodecura : MonoBehaviour
 {
+    [SerializeField] private AudioClip injecaoSound;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-
-        if (collision.CompareTag("Player")) { Destroy(gameObject); }
-
+        if (collision.CompareTag("Player"))
+        {
+            AudioSource.PlayClipAtPoint(injecaoSound, transform.position);
+            Destroy(gameObject);
+        }
     }
 }
--- pretcc/Assets/scripts/coin.cs
+++ pretcc/Assets/scripts/coin.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class coin : MonoBehaviour
 {
     [SerializeField] private int value = 1;
+    [SerializeField] private AudioClip coinSound;
 
-
-    void OnTriggerEnter2D (Collider2D collision){  
-
-        if (collision.CompareTag( "Player" )){ Destroy(gameObject); }
-
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            AudioSource.PlayClipAtPoint(coinSound, transform.position);
+            Destroy(gameObject);
+        }
     }
 }
